81399dfe7e923876649da0b86b1cf91e10b54043
Eliezer Emanuel Veras Vargas <eliezeremanuelvv@gmail.com>
2021-02-27 22:43:12 -0400

    Add testing for feature "Add new reminder"

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,11 +1,53 @@
-import { browser, by, element } from 'protractor';
+// @ts-nocheck
+import { browser} from 'protractor';
 
 export class AppPage {
+
+  calendarItem;
+
   async navigateTo(): Promise<unknown> {
     return browser.get(browser.baseUrl);
   }
 
-  async getTitleText(): Promise<string> {
-    return element(by.css('app-root .content span')).getText();
+  async openReminderForm(): Promise<void> {
+    const items = await $$('#calendar-item');
+    this.calendarItem = await items[items.length - 5];
+    await this.calendarItem.click()
+  }
+
+  async fillReminderForm(): Promise<void> {
+    const descInput = await $('#reminder-description');
+    await descInput.click();
+    await descInput.clear();
+    await descInput.sendKeys('Important meeting with CEO');
+    const value = await descInput.getAttribute('value');
+    expect(value).toEqual('Important meeting with CEO');
+    const cityInput = await $('#reminder-city'); 
+    await cityInput.click();
+    const option = await $('#mat-option-0') ;
+    await option.click();
+    const cityInputText = await cityInput.$('.mat-select-value-text').getText('value');
+    expect(cityInputText).toEqual('New York');
+    const timePicker = await $('.time-picker');
+    await timePicker.click();
+    await timePicker.sendKeys('\uE013');
+    await timePicker.sendKeys('\uE014');
+    await timePicker.sendKeys('\uE013');
+    await timePicker.sendKeys('\uE014');
+    await timePicker.sendKeys('\uE013');
+    const timePikcerText = await timePicker.getAttribute('value');
+    expect(timePikcerText).toEqual('01:00');
+   const saveBtn = await $('#save-button');
+   await saveBtn.click(); 
+  }
+
+  async checkReminderWasCreated(): Promise<void> {
+    setTimeout(this.check);  
+  }
+
+  async check(){
+    const reminder = await this.calendarItem.$('.mat-chip > div');
+    const reminderText = await reminder.getText();
+    expect(reminderText).toEqual('Important meeting with CEO');
   }
 }
